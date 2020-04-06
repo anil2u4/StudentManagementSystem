14e1c082207d4926e142d0a1c5a9e46ff2988376
Anilprasad KV <anilprasad.kv@outlook.com>
2020-04-06 12:41:03 +0530

    Small change

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace StudentManagementSystem.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            // Hai
         }
 
         public IActionResult Index()
