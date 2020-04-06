88231c83fa1a1a44e9173408ceb744d24e9e4a20
Anilprasad KV <anilprasad.kv@outlook.com>
2020-04-06 16:51:09 +0530

    add class

---
+++ IRepository/IStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using StudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.IRepository
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> Get();
+        Task<Student>Get (string id);
+        Task Add(Student Student);
+        Task<string> Update(string id,Student Student);
+        Task<DeleteResult> Remove(string id);
+        Task<DeleteResult> RemoveAll();
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using Bogus.Bson;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Roll { get; set; }
+        public string Email { get; set; }
+
+    }
+}
